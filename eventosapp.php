f0b2ddcea8c7cb86d64feb7b01b3f5c9d1f50ff1
The Lead Partner <manager@theleadpartner.com>
2026-02-04 11:00:29 -0500

    Update eventosapp.php

--- eventosapp.php
+++ eventosapp.php
@@ -48,6 +48,7 @@ require_once plugin_dir_path(__FILE__) . 'includes/admin/eventosapp-auto-network
 require_once plugin_dir_path(__FILE__) . 'includes/admin/eventosapp-event-checklist.php'; // <-- NUEVO
 require_once plugin_dir_path(__FILE__) . 'includes/frontend/eventosapp-ranking-networking.php'; // <-- NUEVO: Ranking de Networking
 require_once plugin_dir_path(__FILE__) . 'includes/admin/eventosapp-batch-refresh.php';   // <-- NUEVO: batch refresh tickets por evento
+require_once plugin_dir_path(__FILE__) . 'includes/admin/eventosapp-batch-processor.php'; // <-- NUEVO: Sistema de actualización por lote v3.0
 require_once plugin_dir_path(__FILE__) . 'includes/functions/eventosapp-webhook-conditionals.php';
 require_once plugin_dir_path(__FILE__) . 'includes/functions/eventosapp-networking-search.php';
 
